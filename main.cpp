f97da6c44f0d30b150feb02fa18370c27257c103
Ann <hezining@sjtu.edu.cn>
2025-05-29 11:00:07 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <random>
 #include <iostream>
 #include "BPT/BPlusTree.h"
+#include "Management/TrainManagement.h"
 #include "Tool/CommandParser.h"
 #include "Tool/MyChar.h"
 #include "Tool/Time.h"
@@ -37,6 +38,7 @@ vector<string> getStr(const string &input) {
 
 vector<int> getInt(const string &input) {
     vector<int> res;
+    res.push_back(0);
     Command temp(input, '|');
     temp.position = 0;
     while (temp.count--) {
@@ -55,7 +57,7 @@ SaleDate getSaleDate(const string &input) {
     temp.position = 0;
     string ans1 = temp.getNext();
     string ans2 = temp.getNext();
-    SaleDate ans(ans1,ans2);
+    SaleDate ans(ans1, ans2);
     return ans;
 }
 
@@ -72,7 +74,8 @@ int main() {
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
 
-    UserManagement usermanagement;
+    UserManagement user_management;
+    TrainManagement train_management;
     string input;
     while (getline(cin, input)) {
         Command command(input);
@@ -83,7 +86,7 @@ int main() {
             MyChar<24> name;
             MyChar<32> mailAddr;
             int privilege = 10;
-            if (!usermanagement.flag) {
+            if (!user_management.flag) {
                 while (command.current < command.count) {
                     string key = command.getNext();
                     if (key == "-u") {
@@ -97,8 +100,9 @@ int main() {
                     }
                     command.current += 2;
                 }
-                usermanagement.flag = true;
-                cout << command.timeStamp << ' ' << usermanagement.add_first_user(username, password, name, mailAddr) << '\n';
+                user_management.flag = true;
+                cout << command.timeStamp << ' ' << user_management.add_first_user(username, password, name, mailAddr)
+                        << '\n';
             } else {
                 while (command.current < command.count) {
                     string key = command.getNext();
@@ -117,7 +121,8 @@ int main() {
                     }
                     command.current += 2;
                 }
-                cout << command.timeStamp << ' ' << usermanagement.add_user(cur_username, username, password, name, mailAddr, privilege) << '\n';
+                cout << command.timeStamp << ' ' << user_management.add_user(
+                    cur_username, username, password, name, mailAddr, privilege) << '\n';
             }
         } else if (command.cmd == "login") {
             MyChar<24> username;
@@ -131,12 +136,12 @@ int main() {
                 }
                 command.current += 2;
             }
-            cout << command.timeStamp << ' ' << usermanagement.login(username, password) << '\n';
+            cout << command.timeStamp << ' ' << user_management.login(username, password) << '\n';
         } else if (command.cmd == "logout") {
             string key = command.getNext();
             MyChar<24> username;
             username = command.getNext();
-            cout << command.timeStamp << ' ' << usermanagement.logout(username) << '\n';
+            cout << command.timeStamp << ' ' << user_management.logout(username) << '\n';
         } else if (command.cmd == "query_profile") {
             MyChar<24> cur_username;
             MyChar<24> username;
@@ -149,10 +154,10 @@ int main() {
                 }
                 command.current += 2;
             }
-            if (!usermanagement.query_profile(cur_username, username).second) {
+            if (!user_management.query_profile(cur_username, username).second) {
                 cout << command.timeStamp << ' ' << -1 << '\n';
             } else {
-                cout << command.timeStamp << ' ' << usermanagement.query_profile(cur_username, username).first << '\n';
+                cout << command.timeStamp << ' ' << user_management.query_profile(cur_username, username).first << '\n';
             }
         } else if (command.cmd == "modify_profile") {
             MyChar<24> cur_username;
@@ -178,10 +183,11 @@ int main() {
                 }
                 command.current += 2;
             }
-            if (!usermanagement.modify_profile(cur_username, username, password, name, mailAddr, privilege).second) {
+            if (!user_management.modify_profile(cur_username, username, password, name, mailAddr, privilege).second) {
                 cout << command.timeStamp << ' ' << -1 << '\n';
             } else {
-                cout << command.timeStamp << ' ' << usermanagement.modify_profile(cur_username, username, password, name, mailAddr, privilege).first
+                cout << command.timeStamp << ' ' << user_management.modify_profile(
+                            cur_username, username, password, name, mailAddr, privilege).first
                         << '\n';
             }
         } else if (command.cmd == "add_train") {
@@ -231,17 +237,19 @@ int main() {
                 }
                 command.current += 2;
             }
-            //TODO
+            cout << command.timeStamp << ' ' << train_management.add_train(
+                trainID, stationNum, seatNum, stations, prices, startTime, travelTimes, stopoverTimes, saleDate,
+                type) << '\n';
         } else if (command.cmd == "delete_train") {
             MyChar<24> trainID;
             string i = command.getNext();
             trainID = command.getNext();
-            //TODO
+            cout << command.timeStamp << ' ' << train_management.delete_train(trainID) << '\n';
         } else if (command.cmd == "release_train") {
             MyChar<24> trainID;
             string i = command.getNext();
             trainID = command.getNext();
-            //TODO
+            cout << command.timeStamp << ' ' << train_management.release_train(trainID) << '\n';
         } else if (command.cmd == "query_train") {
             MyChar<24> trainID;
             Date d;
@@ -256,7 +264,8 @@ int main() {
                 }
                 command.current += 2;
             }
-            //TODO
+            cout << command.timeStamp << ' ';
+            train_management.query_train(trainID, d);
         } else if (command.cmd == "query_ticket") {
             Date d;
             MyChar<24> s;
@@ -278,9 +287,11 @@ int main() {
                 command.current += 2;
             }
             if (p == "time") {
-                //TODO
+                cout << command.timeStamp << ' ';
+                train_management.query_ticket_time(d, s, t);
             } else if (p == "cost") {
-                //TODO
+                cout << command.timeStamp << ' ';
+                train_management.query_ticket_cost(d, s, t);
             }
         } else if (command.cmd == "query_transfer") {
             Date d;
@@ -303,9 +314,9 @@ int main() {
                 command.current += 2;
             }
             if (p == "time") {
-                //TODO
+                train_management.query_transfer_time(d, s, t);
             } else if (p == "cost") {
-                //TODO
+                train_management.query_transfer_cost(d, s, t);
             }
         } else if (command.cmd == "buy_ticket") {
             MyChar<24> username;
@@ -359,8 +370,14 @@ int main() {
             }
             //TODO
         } else if (command.cmd == "clean") {
+            user_management.clean_user_file();
+            cout << 0 << '\n';
             //TODO:清除所有数据
         } else if (command.cmd == "exit") {
+<<<<<<< HEAD
+=======
+            user_management.end();
+>>>>>>> 828068a (basic structure)
             cout << "bye" << '\n';
             //TODO:下线所有用户
             break;
