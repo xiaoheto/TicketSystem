e920ecfdd9b0a7e1db5355fb0e43ba004b256f25
Ann <hezining@sjtu.edu.cn>
2025-05-29 10:57:44 +0800

    Update Ticket.h

--- BasicInfo/Ticket.h
+++ BasicInfo/Ticket.h
@@ -9,22 +9,83 @@
 #include "../Tool/MyChar.h"
 #include "../Tool/Time.h"
 
+enum State {
+    success, pending, refunded
+};
+
+class TicketManagement;
+
 class Ticket {
+    MyChar<24> username;
     MyChar<24> trainID;
-    Date day;//从始发站出发的日期
-    Clock initial_time;//从始发站出发的时间
-    int start_station;
-    int leave_station;
-    int time;
-    int seat;
+    int start_ = 0;
+    int end_ = 0;
+    Date day = Date();
+    State state = success;
+    int time = 0; // 交易时间
+    int num = 0;
+    int index;
+
+    friend class TicketManagement;
 
 public:
     Ticket() = default;
 
-    Ticket(const MyChar<24> &id,const Date &day,const Clock &clock,int st,int en,int time,int seat):
-    trainID(id),day(day),initial_time(clock),start_station(st),leave_station(en),time(time),seat(seat) {
+    Ticket(const MyChar<24> &username, const MyChar<24> &trainID, int start, int end, const Date &date, State state,
+           int time,int num)
+        : username(username), trainID(trainID), start_(start), end_(end), day(date), state(state), time(time),num(num) {
     }
 
     Ticket(const Ticket &other) = default;
+
+    Ticket &operator=(const Ticket &other) {
+        if (this != &other) {
+            username = other.username;
+            trainID = other.trainID;
+            start_ = other.start_;
+            end_ = other.end_;
+            day = other.day;
+            state = other.state;
+            time = other.time;
+        }
+        return *this;
+    }
+
+    bool operator==(const Ticket &other) const {
+        return time == other.time;
+    }
+
+    bool operator<(const Ticket &other) const {
+        return time < other.time;
+    }
+
+    bool operator!=(const Ticket &other) const {
+        return !(*this == other);
+    }
+
+    bool operator>(const Ticket &other) const {
+        return time > other.time;
+    }
+};
+
+class TrainTicketInfo {
+    MyChar<24> trainID;
+    Date day = Date(); // initial_day
+
+public:
+    TrainTicketInfo() = default;
+
+    TrainTicketInfo(const MyChar<24> &trainID, const Date &d): trainID(trainID), day(d) {
+    }
+
+    TrainTicketInfo(const TrainTicketInfo &other) = default;
+
+    TrainTicketInfo &operator=(const TrainTicketInfo &other) {
+        if (this != &other) {
+            trainID = other.trainID;
+            day = other.day;
+        }
+        return *this;
+    }
 };
 #endif //TICKET_H
